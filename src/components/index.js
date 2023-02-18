b430435679c1f304c0388b75f2b2760294fe05bd
tabbee <104757339+tabbep0h@users.noreply.github.com>
2023-02-18 18:46:26 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as HomePage} from "./HomePage"
-export {default as Header} from "./Header"
-export {default as Reg} from "./Reg"
-export {default as Login} from "./Login"
-export {default as Cart} from "./Cart"
-export {default as Order} from "./Order"
-
